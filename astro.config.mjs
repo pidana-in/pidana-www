cf8e6d1798464dea7de2bd23a4940216fc512b54
Abhinav Kushwaha <abhinav@ipsator.com>
2024-09-13 20:06:22 +0530

    bg image fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    domains: ["neon.ipsator.com"],
+  },
   integrations: [
     react(),
     tailwind({
